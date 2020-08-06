aa940713cbaf9906d594192a38c678065eaf7a31
Horacio Vilanculo Junior <38478347+horaciojunior@users.noreply.github.com>
2020-08-06 13:36:33 +0200

    First Commit....

---
+++ app/src/main/java/vilanculo/junior/videocallapp/Authetication/ForgetPasswordActivity.java
@@ -0,0 +1,16 @@
+package vilanculo.junior.videocallapp.Authetication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vilanculo.junior.videocallapp.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
---
+++ app/src/main/java/vilanculo/junior/videocallapp/Authetication/SignInActivity.java
@@ -0,0 +1,16 @@
+package vilanculo.junior.videocallapp.Authetication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vilanculo.junior.videocallapp.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
